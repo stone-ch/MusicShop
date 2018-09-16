f22b57ad0c2d1a38cc9f5e1df952de349ced11d2
stonepc-mi <great_cy_ang@163.com>
2018-09-16 11:51:35 +0800

    add download count

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
 
-  address[16] public adopters;  // 保存领养者的地址
+  uint[16] public adopters;  // 保存领养者的地址
 
     // 领养宠物
   function adopt(uint petId) public returns (uint) {
     require(petId >= 0 && petId <= 15);  // 确保id在数组长度内
 
-    adopters[petId] = msg.sender;        // 保存调用这地址 
+    adopters[petId] += 1;        // 保存调用这地址 
     return petId;
   }
 
   // 返回领养者
-  function getAdopters() public view returns (address[16]) {
+  function getAdopters() public view returns (uint[16]) {
     return adopters;
   }
 
--- src/js/app.js
+++ src/js/app.js
@@ -69,8 +69,9 @@ App = {
       return adoptionInstance.getAdopters.call();
     }).then(function(adopters) {
       for (i = 0; i < adopters.length; i++) {
-        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        if (adopters[i] != 0) {
+          $('.download-count').eq(i).text(adopters[i]);
+          //$('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
     }).catch(function(err) {
